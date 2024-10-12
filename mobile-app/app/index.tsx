4cffb4ff9073330cecf16c7362df1e5432c65ab5
Celine_00 <132836456+MeowCeline00@users.noreply.github.com>
2024-10-11 19:10:19 -0700

    add profile photo

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -1,11 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, Image } from "react-native";
 import MyButton from "@/components/mycustom/atom/Button";
 import React from "react";
 
-
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "white" }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", backgroundColor: "gray" }}>
+        <TouchableOpacity>
+          <Image
+            source={require('@/images/alice001.png')}
+            style={{
+              width: 100,
+              height: 100,
+              borderRadius: 20,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
       <Text>🥰This is Aether, the simple form filling app.</Text>
       <MyButton />
     </View>
